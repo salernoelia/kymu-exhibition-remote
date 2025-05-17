689951cf9915da099762e1f3604c3714a0c783e5
Elia Salerno <putopet@gmail.com>
2025-05-17 11:46:12 +0200

    feat: three buttons

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,23 @@
 #include <BleKeyboard.h>
 
 #define LED_PIN 22
-#define BUTTON_PIN 15
+#define BUTTON_ONE_PIN 15
+#define BUTTON_TWO_PIN 18
+#define BUTTON_THREE_PIN 19
 
 BleKeyboard bleKeyboard;
 
 void blinkLed();
+bool lastButtonOneState = HIGH; 
+bool lastButtonTwoState = HIGH; 
+bool lastButtonThreeState = HIGH; 
 
 void setup() 
 {
   pinMode(LED_PIN, OUTPUT);    
-  pinMode(BUTTON_PIN, INPUT_PULLUP); 
+  pinMode(BUTTON_ONE_PIN, INPUT_PULLUP); 
+  pinMode(BUTTON_TWO_PIN, INPUT_PULLUP); 
+  pinMode(BUTTON_THREE_PIN, INPUT_PULLUP); 
 
   Serial.begin(115200);
   bleKeyboard.begin();
@@ -19,18 +26,31 @@ void setup()
 
 void loop() 
 {
-  static bool lastButtonState = HIGH; 
-  bool currentButtonState = digitalRead(BUTTON_PIN);
+  bool currentButtonOneState = digitalRead(BUTTON_ONE_PIN);
+  bool currentButtonTwoState = digitalRead(BUTTON_TWO_PIN);
+  bool currentButtonThreeState = digitalRead(BUTTON_THREE_PIN);
 
   if (bleKeyboard.isConnected()) {
-    if (lastButtonState == HIGH && currentButtonState == LOW) 
+    if (lastButtonOneState == HIGH && currentButtonOneState == LOW) 
     {
-      bleKeyboard.write(KEY_NUM_0);
+      bleKeyboard.write(KEY_NUM_1);
+      blinkLed();
+    }
+    if (lastButtonTwoState == HIGH && currentButtonTwoState == LOW) 
+    {
+      bleKeyboard.write(KEY_NUM_2);
+      blinkLed();
+    }
+    if (lastButtonThreeState == HIGH && currentButtonThreeState == LOW) 
+    {
+      bleKeyboard.write(KEY_NUM_3);
       blinkLed();
     }
   }
 
-  lastButtonState = currentButtonState; 
+  lastButtonOneState = currentButtonOneState; 
+  lastButtonTwoState = currentButtonTwoState; 
+  lastButtonThreeState = currentButtonThreeState; 
   delay(10);
 }
 
